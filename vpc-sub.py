855a9b9414464e7b2171e51c327594d3a3e469ee
Evandro Couto <ecouto@icasei.com.br>
2017-10-06 16:28:11 -0300

    aws cli

--- vpc-sub.py
+++ vpc-sub.py
@@ -13,6 +13,7 @@ VPCiD=0
 DirProjVPC="ec2/projeto1/vpc.tf"
 DirProjSUB="ec2/projeto1/subnets.tf"
 
+
 # generate vpc.tf
 json_data=open("vpc/aws-vpc.json").read()
 data = json.loads(json_data)
@@ -31,15 +32,23 @@ default = [%s]\n\
 # generate subnets.tf
 json_data=open("sn/aws-sn.json").read()
 data = json.loads(json_data)
-subnets='"%s", "%s", "%s", "%s"' \
-        %(data["Subnets"][VPCiD]["SubnetId"], data["Subnets"][1]["SubnetId"], data["Subnets"][2]["SubnetId"], data["Subnets"][3]["SubnetId"])
-print subnets
 
+
+num=(len(data["Subnets"]))
+count=0
+list=[]
+subnets=""
+while ( count < num):
+   subnets+= '"%s", '  %(str((data["Subnets"][count]["SubnetId"])))
+   count = count + 1
 
 file = open(DirProjSUB, "w")
 file.write('variable "subnet" {\n\
 type = "list"\n\
 default = [%s]\n\
 }\n\
-' %(subnets
+' %(subnets[0:-2]
    ))
+
+
+print subnets[0:-2]
